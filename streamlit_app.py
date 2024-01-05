7cd8d8c376393ff880f5a267bb358ca49072665f
Rahul Sundar <rahul.sundar95@gmail.com>
2024-01-05 18:41:37 +0530

    SOURCE: File structure update.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ import tiktoken
 encoding = tiktoken.get_encoding("cl100k_base")
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 # -------------IMPORTING CORE FUNCTIONALITIES OF THE SpeeKAR_BOT-------------
-from src.embeddinggenerator import *
-from src.chatbotfunctions import chatbot
+from embeddinggenerator import *
+from chatbotfunctions import chatbot
 
 # -------------------AUDIO FUNCTIONALITY-------------------------
 from mutagen.wave import WAVE
